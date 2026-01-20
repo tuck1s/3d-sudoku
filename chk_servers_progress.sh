e810f28216e9b778a9cb144c23252fc16778473d
tuck1s <steve@thetucks.com>
2026-01-20 17:46:19 +0000

    comment out finished server

--- chk_servers_progress.sh
+++ chk_servers_progress.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # ssh steve@steve-xps-8900.local "cd 3d-sudoku && ./summarize_all.py"
 ssh root@172.237.100.153 "cd 3d-sudoku && ./summarize_all.py"
-ssh root@172.236.28.219  "cd 3d-sudoku && ./summarize_all.py"
+# sudoku2 server has now finished ..
+# ssh root@172.236.28.219  "cd 3d-sudoku && ./summarize_all.py"
